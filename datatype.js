614a879094c77ede91d620a12e192c22abccab81
hemabindu <hemabinduchirra@gmail.com>
2025-09-22 11:02:07 +0530

    datatype.js

---
+++ datatype.js
@@ -0,0 +1,24 @@
+a = 7897;
+console.log(typeof a);
+name = "hema";
+console.log(typeof name);
+c = true;
+console.log(typeof c);
+data = null;
+console.log(typeof data);
+d = undefined;
+console.log(typeof d);
+
+//non primitive
+array = [56, "hema", true, null];
+console.log(typeof array);
+obj = {
+    name: "hema",
+    age: 23,
+    dept: "AIML"
+};
+console.log(typeof obj);
+
+
+
+
