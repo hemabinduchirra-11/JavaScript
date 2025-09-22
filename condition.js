7f81a2ac5baa1ea67ba8275845f3624a8bdb4793
hema <hemabinduchirra@gmail.com>
2025-09-22 16:27:04 +0530

    update changes

---
+++ condition.js
@@ -0,0 +1,6 @@
+ a = prompt("enetr the age");
+if (a < 18) {
+    document.write("This is not for you! Your are not eligible for training");
+} else {
+        document.write("Welcome to the dashboard");
+    }
